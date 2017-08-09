23fcb83798079ea404f267615ae4e8dd0d6261c3
Sean McRae <officialhocc@gmail.com>
2017-08-08 20:27:36 -0400

    Checkpoint 1

---
+++ app/fixtures.js
@@ -0,0 +1,29 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'album/01.png',
+     songs: [
+       { title: 'Blue', duration: '4:26', audioUrl: 'music/blue' },
+       { title: 'Green', duration: '3:14', audioUrl: 'music/green' },
+       { title: 'Red', duration: '5:01', audioUrl: 'music/red' },
+       { title: 'Pink', duration: '3:21', audioUrl: 'music/pink' },
+       { title: 'Magenta', duration: '2:15', audioUrl: 'music/magenta' }
+     ]
+ };
+
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'album/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21' },
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15' }
+     ]
+ };
